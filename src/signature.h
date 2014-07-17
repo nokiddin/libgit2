bb708e1e9d496a4b7b412e774745115556c71ea1
Edward Thomson <ethomson@edwardthomson.com>
2014-07-17 18:25:03 -0400

    Introduce git_rebase_commit
    Commit the current patch of a rebase process.

--- src/signature.h
+++ src/signature.h
@@ -14,5 +14,6 @@
 
 int git_signature__parse(git_signature *sig, const char **buffer_out, const char *buffer_end, const char *header, char ender);
 void git_signature__writebuf(git_buf *buf, const char *header, const git_signature *sig);
+bool git_signature__equal(const git_signature *one, const git_signature *two);
 
 #endif
